cd05b3f16616eca127f0340885dcf5712396f669
Zoran Lazarevic <zoranlazarevic@Zorans-MacBook-Pro.local>
2023-05-25 14:56:49 +0200

    added condition

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,37 +2,47 @@ import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import client from "../../config/client";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
+import { NoFallbackError } from "next/dist/server/base-server";
 
 const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
   console.log(post);
   return (
-    <article className="h-screen w-screen flex justify-center">
-      <div className="gap-4 flex flex-col w-full">
-        <div className="w-full h-1/3 relative">
-          <Image
-            src={post.imageUrl}
-            fill
-            alt={post.title}
-            loading="lazy"
-            blurDataURL={post.imageUrl}
-            placeholder="blur"
-            className="w-full h-80 object-cover rounded-md"
-          />
-        </div>
-        <div className="flex flex-col w-full p-8">
-          <p className="font-mono text-sm  text-gray-400">
-            Published: {post.created}
-          </p>
-          <p className="font-mono text-sm  text-gray-400">
-            Autor: {post.author}
-          </p>
-          <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 my-4">
-            {post.title}
-          </h1>
-          <PortableText value={post.text} />
-        </div>
-      </div>
-    </article>
+    <>
+      {post ? (
+        <>
+          {" "}
+          <article className="h-screen w-screen flex justify-center">
+            <div className="gap-4 flex flex-col w-full">
+              <div className="w-full h-1/3 relative">
+                <Image
+                  src={post.imageUrl}
+                  fill
+                  alt={post.title}
+                  loading="lazy"
+                  blurDataURL={post.imageUrl}
+                  placeholder="blur"
+                  className="w-full h-80 object-cover rounded-md"
+                />
+              </div>
+              <div className="flex flex-col w-full p-8">
+                <p className="font-mono text-sm  text-gray-400">
+                  Published: {post.created}
+                </p>
+                <p className="font-mono text-sm  text-gray-400">
+                  Autor: {post.author}
+                </p>
+                <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 my-4">
+                  {post.title}
+                </h1>
+                <PortableText value={post.text} />
+              </div>
+            </div>
+          </article>
+        </>
+      ) : (
+        <h3>nothing to show</h3>
+      )}
+    </>
   );
 };
 
